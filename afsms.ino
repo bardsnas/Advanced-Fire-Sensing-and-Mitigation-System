e1ed972477acea22cb5d798bcfae2519833e4350
bardsnas <bards.nas@gmail.com>
2025-03-13 21:53:09 -0700

    Update afsms.ino
    Implementing the Wind detector task

--- afsms.ino
+++ afsms.ino
@@ -20,6 +20,8 @@
 #define CRITICAL 17
 #define EMERGENCY 18
 
+#define POT_PIN 6
+
 // I2C communcation: Initialize 16x2 LCD (I2C address: 0x27, 16 columns, 2 rows)
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -31,19 +33,21 @@ TaskHandle_t Temp_Humid_Handle = NULL;
 TaskHandle_t Rain_Handle = NULL;
 TaskHandle_t Wind_Handle = NULL;
 
-// Degree of seriousness handles
-TaskHandle_t Degree_Handle = NULL;
+// FWI Calculation handle
+TaskHandle_t FWI_Calc_Handle = NULL;
 
 // Detection tasks prototypes
 void Temp_Humid_Det(void *pvParameter);
 void Wind_Det(void *pvParameter);
 void Rain_Det(void *pvParameter);
 
-// Blinking LED tasks prototypes
-void LED_Blink(void *pvParameter);
+// FWI Calculation task prototype
+void FWI_Calc(void *pvParameter);
 
 // ============================= Global Variables =============================
-volatile int brightness = 0;   // Current LED brightness
+volatile int wind = 0;
+volatile float temp = 0;
+volatile float humid = 0;
 volatile int FWI = 0; // Fire Weather Index
 
 void setup() {
@@ -54,6 +58,7 @@ void setup() {
   // Initialize pins
   pinMode(TEMP_LED, OUTPUT);
   pinMode(HUMID_LED, OUTPUT);
+  pinMode(POT_PIN, INPUT);
 
   // Initialize I2C communication
   Wire.begin();
@@ -68,7 +73,8 @@ void setup() {
   // Create tasks
   // TODO:...
   xTaskCreate(Temp_Humid_Det, "Temperture & Humidity", 4096, NULL, 1, &Temp_Humid_Handle);
-
+  xTaskCreate(Wind_Det, "Wind", 4096, NULL, 1, &Wind_Handle);
+  xTaskCreate(FWI_Calc, "FWI", 4096, NULL, 1, &FWI_Calc_Handle);
 }
 
 // Create Tasks Functions
@@ -76,16 +82,33 @@ void setup() {
 // Temperature and Humidity sensing
 void Temp_Humid_Det(void *pvParameter) {
   while (1) {  // Infinite loop to keep the task running
-    float temp = am2320.readTemperature();
-    float humid = am2320.readHumidity();
+    temp = am2320.readTemperature();
+    humid = am2320.readHumidity();
 
-    int brightnessTemp = map(temp, -40, 80, 0, 255);
-    int brightnessHumid = map(humid, 0, 100, 0, 255);  // Fix: Should use 'humid' instead of 'temp'
+    vTaskDelay(pdMS_TO_TICKS(500));  // Delay to prevent CPU overload
+  }
+}
 
-    analogWrite(TEMP_LED, brightnessTemp);
-    analogWrite(HUMID_LED, brightnessHumid);
+void Wind_Det(void *pvParameter) {
+  while (1) {
+    int potValue = analogRead(POT_PIN);
+    wind = (potValue / 4095.0) * 50.0;   // Scale value between 0-50 km/h
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
 
-    vTaskDelay(pdMS_TO_TICKS(1000));  // Delay to prevent CPU overload
+void FWI_Calc(void *pvParameter) {
+  while (1) {
+    Serial.print("Wind: ");
+    Serial.println(wind);
+
+    Serial.print("Temp: ");
+    Serial.println(temp);
+
+    Serial.print("Humidity: ");
+    Serial.println(humid);
+    
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
